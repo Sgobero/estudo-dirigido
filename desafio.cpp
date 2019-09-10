8f38b9328f717260c4ac7af8f7a0f78c41752700
Sgobero <55161278+Sgobero@users.noreply.github.com>
2019-09-10 17:34:12 -0300

    Add files via upload

---
+++ desafio.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+
+
+    string parar, caso;
+    vector <int> lista;
+    int nu;
+
+
+
+
+        do{
+
+
+
+
+
+
+
+
+
+
+
+        }while("sim");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        cout << "Digite [0] para sair da aplicação" <<endl;
+        cout << "Digite [1] para imprimir o somatório de seus elementos" <<endl;
+        cout << "Digite [2] para imprimir a média de seus elementos" <<endl;
+        cout << "Digite [3] para imprimir a média e o somatório" <<endl;
+        cout << "Digite [4] substituir por zero todos os valores negativos e imprimir a média" <<endl;
+        cout << "Digite [5] para substituir por zero todos os valores repetidos e imprimir a média e o somatório" <<endl;
+        cout << "Digite [6] mostrar o vetor ordenado" <<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
+
+
